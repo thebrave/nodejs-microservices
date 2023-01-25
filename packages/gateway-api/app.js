08b556b77b6a7d53b7e88c016dc32540a3e48f70
Jean Berniolles <jean@berniolles.fr>
2023-01-25 13:59:44 +0000

    end of step 8

--- packages/gateway-api/app.js
+++ packages/gateway-api/app.js
@@ -3,6 +3,7 @@ const cookieParser = require('cookie-parser');
 const pino = require('pino-http')();
 
 const router = require('./routes/index');
+const auth = require('./middlewares/auth');
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(pino);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(auth);
 
 app.use('/api', router);
 
